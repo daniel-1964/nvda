b04efcba9a0d2b8fdc222330427e446892074de9
daniel-1964 <daniel.cotto@gmail.com>
2016-10-10 18:53:01 +0200

    Improve FreedomScientific Braille Display Driver
    * Add firmness setting
    * Add some keyboard emulation hotkeys
    * Add some NVDA command hotkeys

--- source/brailleDisplayDrivers/freedomScientific.py
+++ source/brailleDisplayDrivers/freedomScientific.py
@@ -32,6 +32,7 @@ if fsbLib:
 	fbGetDisplayName=getattr(fsbLib, "_fbGetDisplayName@12")
 	fbGetFirmwareVersion=getattr(fsbLib,  "_fbGetFirmwareVersion@12")
 	fbBeep=getattr(fsbLib, "_fbBeep@4")
+	fbSetVariBraille=getattr(fsbLib,'_fbSetVariBraille@8')
 
 FB_INPUT=1
 FB_DISCONNECT=2
@@ -39,6 +40,13 @@ FB_EXT_KEY=3
 
 LRESULT=c_long
 HCURSOR=c_long
+
+# Firmness setting
+FB_FIRMNESS_MINIMUM=0x00
+FB_FIRMNESS_LOW=1
+FB_FIRMNESS_MEDIUM=2
+FB_FIRMNESS_HIGH=3
+FB_FIRMNESS_MAXIMUM=4
 
 appInstance=windll.kernel32.GetModuleHandleW(None)
 
@@ -117,6 +125,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver,ScriptableObject):
 	name="freedomScientific"
 	# Translators: Names of braille displays.
 	description=_("Freedom Scientific Focus/PAC Mate series")
+	firmness=FB_FIRMNESS_MAXIMUM
 
 	@classmethod
 	def check(cls):
@@ -170,6 +179,7 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver,ScriptableObject):
 			portsToTry = ["USB"]
 		fbHandle=-1
 		for port in portsToTry:
+                        log.debug("Trying port %s", port)
 			fbHandle=fbOpen(port,self._messageWindow,nvdaFsBrlWm)
 			if fbHandle!=-1:
 				break
@@ -180,8 +190,14 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver,ScriptableObject):
 		self.fbHandle=fbHandle
 		self._configureDisplay()
 		numCells=self.numCells
-		self.gestureMap.add("br(freedomScientific):topRouting1","globalCommands","GlobalCommands","braille_scrollBack")
-		self.gestureMap.add("br(freedomScientific):topRouting%d"%numCells,"globalCommands","GlobalCommands","braille_scrollForward")
+		for i in xrange(1, 6):
+			self.gestureMap.add("br(freedomScientific):topRouting%d"%i,"globalCommands","GlobalCommands","braille_scrollBack")
+		for i in xrange(numCells, numCells - 5, -1):
+			self.gestureMap.add("br(freedomScientific):topRouting%d"%i,"globalCommands","GlobalCommands","braille_scrollForward")
+		for i in xrange((numCells / 2) + 1, (numCells / 2) + 6):
+			self.gestureMap.add("br(freedomScientific):topRouting%d"%i,"globalCommands","GlobalCommands","braille_nextLine")
+		for i in xrange(numCells / 2, (numCells / 2) - 5, -1):
+			self.gestureMap.add("br(freedomScientific):topRouting%d"%i,"globalCommands","GlobalCommands","braille_previousLine")
 
 	def terminate(self):
 		super(BrailleDisplayDriver,self).terminate()
@@ -208,6 +224,8 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver,ScriptableObject):
 			# Focus 2 or later. Make sure extended keys support is enabled.
 			log.debug("Activating extended keys on freedom Scientific display. Display name: %s, firmware version: %s.", displayName, firmwareVersion)
 			fbConfigure(self.fbHandle, 0x02)
+			log.debug("Setting firmness to %d on freedom Scientific display. Display name: %s, firmwareversion: %s.", self.firmness, displayName, firmwareVersion)
+			fbSetVariBraille(self.fbHandle, self.firmness * 0xff / FB_FIRMNESS_MAXIMUM)
 
 	def script_toggleLeftWizWheelAction(self,gesture):
 		action=self.leftWizWheelActionCycle.next()
@@ -233,6 +251,160 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver,ScriptableObject):
 			"braille_scrollForward" : ("br(freedomScientific):rightAdvanceBar","br(freedomScientific):leftBumperBarDown","br(freedomScientific):rightBumperBarDown",),
 			"braille_previousLine" : ("br(freedomScientific):leftRockerBarUp", "br(freedomScientific):rightRockerBarUp",),
 			"braille_nextLine" : ("br(freedomScientific):leftRockerBarDown", "br(freedomScientific):rightRockerBarDown",),
+			"kb:alt+a" : ("br(freedomscientific):dot1+rightShiftKey",),
+			"kb:alt+b" : ("br(freedomscientific):dot2+dot1+rightShiftKey",),
+			"kb:alt+c" : ("br(freedomscientific):dot1+dot4+rightShiftKey",),
+			"kb:alt+d" : ("br(freedomscientific):dot1+dot4+dot5+rightShiftKey",),
+			"kb:alt+e" : ("br(freedomscientific):dot1+dot5+rightShiftKey",),
+			"kb:alt+f" : ("br(freedomscientific):dot1+dot2+dot4+rightShiftKey",),
+			"kb:alt+g" : ("br(freedomscientific):dot2+dot1+rightShiftKey+dot5+dot4",),
+			"kb:alt+h" : ("br(freedomscientific):dot2+dot1+rightShiftKey+dot5",),
+			"kb:alt+i" : ("br(freedomscientific):dot2+rightShiftKey+dot4",),
+			"kb:alt+j" : ("br(freedomscientific):dot2+rightShiftKey+dot5+dot4",),
+			"kb:alt+k" : ("br(freedomscientific):dot3+dot1+rightShiftKey",),
+			"kb:alt+l" : ("br(freedomscientific):dot3+dot2+dot1+rightShiftKey",),
+			"kb:alt+m" : ("br(freedomscientific):dot3+dot1+rightShiftKey+dot4",),
+			"kb:alt+n" : ("br(freedomscientific):dot3+d ot1+rightShiftKey+dot5+dot4",),
+			"kb:alt+o" : ("br(freedomscientific):dot3+dot1+rightShiftKey+dot5",),
+			"kb:alt+p" : ("br(freedomscientific):dot3+dot2+dot1+rightShiftKey+dot4",),
+			"kb:alt+q" : ("br(freedomscientific):dot3+dot2+dot1+dot5+rightShiftKey+dot4",),
+			"kb:alt+r" : ("br(freedomscientific):dot3+dot2+dot1+rightShiftKey+dot5",),
+			"kb:alt+s" : ("br(freedomscientific):dot3+dot2+rightShiftKey+dot4",),
+			"kb:alt+t" : ("br(freedomscientific):dot3+dot2+rightShiftKey+dot5+dot4",),
+			"kb:alt+u" : ("br(freedomscientific):dot3+dot1+dot6+rightShiftKey",),
+			"kb:alt+v" : ("br(freedomscientific):dot3+dot2+dot1+dot6+rightShiftKey",),
+			"kb:alt+w" : ("br(freedomscientific):dot2+rightShiftKey+dot6+dot5+dot4",),
+			"kb:alt+x" : ("br(freedomscientific):dot3+dot1+dot6+rightShiftKey+dot4",),
+			"kb:alt+y" : ("br(freedomscientific):dot3+dot1+dot6+rightShiftKey+dot4+dot5",),
+			"kb:alt+z" : ("br(freedomscientific):dot3+dot1+rightShiftKey+dot6+dot5",),
+			"kb:control+a" : ("br(freedomscientific):dot1+dot8+dot7",),
+			"kb:control+b" : ("br(freedomscientific):dot2+dot1+dot8+dot7",),
+			"kb:control+c" : ("br(freedomscientific):dot1+dot8+dot7+dot4",),
+			"kb:control+d" : ("br(freedomscientific):dot1+dot8+dot7+dot5+dot4",),
+			"kb:control+e" : ("br(freedomscientific):dot1+dot8+dot7+dot5",),
+			"kb:control+f" : ("br(freedomscientific):dot2+dot1+dot8+dot7+dot4",),
+			"kb:control+g" : ("br(freedomscientific):dot2+dot1+dot7+dot5+dot4+dot8",),
+			"kb:control+h" : ("br(freedomscientific):dot2+dot1+dot8+dot7+dot5",),
+			"kb:control+i" : ("br(freedomscientific):dot2+dot8+dot7+dot4",),
+			"kb:control+j" : ("br(freedomscientific):dot2+dot8+dot7+dot5+dot4",),
+			"kb:control+k" : ("br(freedomscientific):dot3+dot1+dot8+dot7",),
+			"kb:control+l" : ("br(freedomscientific):dot3+dot2+dot1+dot8+dot7",),
+			"kb:control+m" : ("br(freedomscientific):dot3+dot1+dot8+dot7+dot4",),
+			"kb:control+n" : ("br(freedomscientific):dot3+dot1+dot7+dot5+dot4+dot8",),
+			"kb:control+o" : ("br(freedomscientific):dot3+dot1+dot8+dot7+dot5",),
+			"kb:control+p" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot4+dot8",),
+			"kb:control+q" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot5+dot4+dot8",),
+			"kb:control+r" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot5+dot8",),
+			"kb:control+s" : ("br(freedomscientific):dot3+dot2+dot8+dot7+dot4",),
+			"kb:control+t" : ("br(freedomscientific):dot3+dot2+dot7+dot5+dot4+dot8",),
+			"kb:control+u" : ("br(freedomscientific):dot3+dot1+dot8+dot7+dot6",),
+			"kb:control+v" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot6+dot8",),
+			"kb:control+w" : ("br(freedomscientific):dot2+dot7+dot6+dot5+dot4+dot8",),
+			"kb:control+x" : ("br(freedomscientific):dot3+dot1+dot7+dot6+dot4+dot8",),
+			"kb:control+y" : ("br(freedomscientific):dot3+dot1+dot7+dot6+dot5+dot4+dot8",),
+			"kb:control+z" : ("br(freedomscientific):dot3+dot1+dot7+dot6+dot5+dot8",),
+			"kb:f1" : ("br(freedomscientific):dot1+leftshiftkey",),
+			"kb:f2" : ("br(freedomscientific):dot2+dot1+leftshiftkey",),
+			"kb:f3" : ("br(freedomscientific):dot1+leftshiftkey+dot4",),
+			"kb:f4" : ("br(freedomscientific):dot1+leftshiftkey+dot5+dot4",),
+			"kb:f5" : ("br(freedomscientific):dot1+leftshiftkey+dot5",),
+			"kb:f6" : ("br(freedomscientific):dot2+dot1+leftshiftkey+dot4",),
+			"kb:f7" : ("br(freedomscientific):dot2+dot1+leftshiftkey+dot5+dot4",),
+			"kb:f8" : ("br(freedomscientific):dot2+dot1+leftshiftkey+dot5",),
+			"kb:f9" : ("br(freedomscientific):dot2+leftshiftkey+dot4",),
+			"kb:f10" : ("br(freedomscientific):dot2+leftshiftkey+dot5+dot4",),
+			"kb:f11" : ("br(freedomscientific):dot3+dot1+leftshiftkey",),
+			"kb:f12" : ("br(freedomscientific):dot3+dot2+dot1+leftshiftkey",),
+			"kb:shift+f1" : ("br(freedomscientific):dot1+dot7+leftshiftkey",),
+			"kb:shift+f2" : ("br(freedomscientific):dot2+dot1+dot7+leftshiftkey",),
+			"kb:shift+f3" : ("br(freedomscientific):dot1+dot7+leftshiftkey+dot4",),
+			"kb:shift+f4" : ("br(freedomscientific):dot1+dot7+leftshiftkey+dot5+dot4",),
+			"kb:shift+f5" : ("br(freedomscientific):dot1+dot7+leftshiftkey+dot5",),
+			"kb:shift+f6" : ("br(freedomscientific):dot2+dot1+dot7+leftshiftkey+dot4",),
+			"kb:shift+f7" : ("br(freedomscientific):dot2+dot1+dot7+dot5+dot4+leftshiftkey",),
+			"kb:shift+f8" : ("br(freedomscientific):dot2+dot1+dot7+leftshiftkey+dot5",),
+			"kb:shift+f9" : ("br(freedomscientific):dot2+leftshiftkey+dot7+dot4",),
+			"kb:shift+f10" : ("br(freedomscientific):dot2+leftshiftkey+dot7+dot5+dot4",),
+			"kb:shift+f11" : ("br(freedomscientific):dot3+dot1+dot7+leftshiftkey",),
+			"kb:shift+f12" : ("br(freedomscientific):dot3+dot2+dot1+dot7+leftshiftkey",),
+			"kb:control+f1" : ("br(freedomscientific):dot1+dot8+dot7+leftshiftkey",),
+			"kb:control+f2" : ("br(freedomscientific):dot2+dot1+dot8+dot7+leftshiftkey",),
+			"kb:control+f3" : ("br(freedomscientific):dot1+dot8+dot7+leftshiftkey+dot4",),
+			"kb:control+f4" : ("br(freedomscientific):dot1+dot7+dot5+dot4+dot8+leftshiftkey",),
+			"kb:control+f5" : ("br(freedomscientific):dot1+dot8+dot7+leftshiftkey+dot5",),
+			"kb:control+f6" : ("br(freedomscientific):dot2+dot1+dot7+dot4+dot8+leftshiftkey",),
+			"kb:control+f7" : ("br(freedomscientific):dot2+dot1+dot7+dot5+dot4+dot8+leftshiftkey",),
+			"kb:control+f8" : ("br(freedomscientific):dot2+dot1+dot7+dot5+dot8+leftshiftkey",),
+			"kb:control+f9" : ("br(freedomscientific):dot2+leftshiftkey+dot8+dot7+dot4",),
+			"kb:control+f10" : ("br(freedomscientific):dot2+dot7+dot5+dot4+dot8+leftshiftkey",),
+			"kb:control+f11" : ("br(freedomscientific):dot3+dot1+dot8+dot7+leftshiftkey",),
+			"kb:control+f12" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot8+leftshiftkey",),
+			"kb:alt+f1" : ("br(freedomscientific):dot1+dot8+leftshiftkey",),
+			"kb:alt+f2" : ("br(freedomscientific):dot2+dot1+dot8+leftshiftkey",),
+			"kb:alt+f3" : ("br(freedomscientific):dot1+dot8+leftshiftkey+dot4",),
+			"kb:alt+f4" : ("br(freedomscientific):dot1+dot8+leftshiftkey+dot5+dot4",),
+			"kb:alt+f5" : ("br(freedomscientific):dot1+dot8+leftshiftkey+dot5",),
+			"kb:alt+f6" : ("br(freedomscientific):dot2+dot1+dot8+leftshiftkey+dot4",),
+			"kb:alt+f7" : ("br(freedomscientific):dot2+dot1+dot5+dot4+dot8+leftshiftkey",),
+			"kb:alt+f8" : ("br(freedomscientific):dot2+dot1+dot8+leftshiftkey+dot5",),
+			"kb:alt+f9" : ("br(freedomscientific):dot2+leftshiftkey+dot8+dot4",),
+			"kb:alt+f10" : ("br(freedomscientific):dot2+leftshiftkey+dot8+dot5+dot4",),
+			"kb:alt+f11" : ("br(freedomscientific):dot3+dot1+dot8+leftshiftkey",),
+			"kb:alt+f12" : ("br(freedomscientific):dot3+dot2+dot1+dot8+leftshiftkey",),
+			"kb:backspace" : ("br(freedomscientific):leftshiftkey", "br(freedomScientific):dot7",),
+			"kb:shift+alt+tab" : ("br(freedomscientific):dot2+dot1+dot6+dot5+braillespacebar",),
+			"kb:enter" : ("br(freedomscientific):dot3+braillespacebar+dot5+dot4", "br(freedomscientific):leftshiftkey+rightShiftKey", "br(freedomScientific):dot8",),
+			"kb:shift+enter" : ("br(freedomscientific):dot3+braillespacebar+dot7+dot5+dot4", "br(freedomscientific):leftshiftkey+dot7+rightShiftKey",),
+			"kb:control+enter" : ("br(freedomscientific):dot3+dot7+dot5+dot4+braillespacebar+dot8", "br(freedomscientific):leftshiftkey+dot8+dot7+rightShiftKey",),
+			"kb:alt+enter" : ("br(freedomscientific):dot3+braillespacebar+dot8+dot5+dot4", "br(freedomscientific):leftshiftkey+dot8+rightShiftKey",),
+			"kb:escape" : ("br(freedomscientific):dot3+braillespacebar+dot1+dot6+dot5",),
+			"kb:shift+escape" : ("br(freedomscientific):braillespacebar+dot1+dot7+dot5", "br(freedomscientific):dot3+dot1+dot7+dot6+dot5+braillespacebar",),
+			"kb:control+escape" : ("br(freedomscientific):braillespacebar+dot1+dot8+dot7+dot5", "br(freedomscientific):dot3+dot1+dot7+dot6+dot5+braillespacebar+dot8",),
+			"kb:alt+escape" : ("br(freedomscientific):braillespacebar+dot1+dot8+dot5", "br(freedomscientific):dot3+dot1+dot6+dot5+braillespacebar+dot8",),
+			"kb:shift+space" : ("br(freedomscientific):braillespacebar+dot7",),
+			"kb:control+space" : ("br(freedomscientific):braillespacebar+dot8+dot7",),
+			"kb:alt+space" : ("br(freedomscientific):braillespacebar+dot8",),
+			"kb:shift+upArrow" : ("br(freedomscientific):braillespacebar+dot1+dot7",),
+			"kb:control+upArrow" : ("br(freedomscientific):braillespacebar+dot1+dot8+dot7",),
+			"kb:alt+upArrow" : ("br(freedomscientific):braillespacebar+dot1+dot8",),
+			"kb:shift+downArrow" : ("br(freedomscientific):braillespacebar+dot7+dot4",),
+			"kb:control+downArrow" : ("br(freedomscientific):braillespacebar+dot8+dot7+dot4",),
+			"kb:alt+downArrow" : ("br(freedomscientific):braillespacebar+dot8+dot4",),
+			"kb:shift+leftArrow" : ("br(freedomscientific):dot3+braillespacebar+dot7",),
+			"kb:alt+leftArrow" : ("br(freedomscientific):braillespacebar+dot3+dot8",),
+			"kb:control+shift+leftArrow" : ("br(freedomscientific):braillespacebar+dot2+dot7",),
+			"kb:control+alt+leftArrow" : ("br(freedomscientific):braillespacebar+dot8+dot2",),
+			"kb:shift+rightArrow" : ("br(freedomscientific):braillespacebar+dot7+dot6",),
+			"kb:alt+rightArrow" : ("br(freedomscientific):braillespacebar+dot8+dot6",),
+			"kb:control+shift+rightArrow" : ("br(freedomscientific):braillespacebar+dot7+dot5",),
+			"kb:control+alt+rightArrow" : ("br(freedomscientific):braillespacebar+dot8+dot5",),
+			"kb:shift+home" : ("br(freedomscientific):dot3+braillespacebar+dot1+dot7",),
+			"kb:alt+home" : ("br(freedomscientific):dot3+braillespacebar+dot1+dot8",),
+			"kb:control+shift+home" : ("br(freedomscientific):braillespacebar+dot3+dot2+dot1+dot7",),
+			"kb:control+alt+home" : ("br(freedomscientific):dot3+braillespacebar+dot1+dot8+dot2",),
+			"kb:shift+end" : ("br(freedomscientific):braillespacebar+dot7+dot6+dot4",),
+			"kb:alt+end" : ("br(freedomscientific):braillespacebar+dot8+dot6+dot4",),
+			"kb:control+shift+end" : ("br(freedomscientific):braillespacebar+dot7+dot6+dot5+dot4",),
+			"kb:control+alt+end" : ("br(freedomscientific):braillespacebar+dot8+dot6+dot5+dot4",),
+			"kb:pageUp" : ("br(freedomscientific):braillespacebar+dot3+dot2",),
+			"kb:shift+pageUp" : ("br(freedomscientific):braillespacebar+dot3+dot2+dot7",),
+			"kb:control+pageUp" : ("br(freedomscientific):braillespacebar+dot3+dot2+dot7+dot8",),
+			"kb:alt+pageUp" : ("br(freedomscientific):dot3+braillespacebar+dot8+dot2",),
+			"kb:pageDown" : ("br(freedomscientific):braillespacebar+dot6+dot5",),
+			"kb:shift+pageDown" : ("br(freedomscientific):braillespacebar+dot7+dot6+dot5",),
+			"kb:control+pageDown" : ("br(freedomscientific):braillespacebar+dot8+dot7+dot6+dot5",),
+			"kb:alt+pageDown" : ("br(freedomscientific):braillespacebar+dot8+dot6+dot5",),
+			"kb:insert" : ("br(freedomscientific):braillespacebar+dot2+dot4",),
+			"kb:shift+insert" : ("br(freedomscientific):braillespacebar+dot2+dot4+dot7",),
+			"kb:control+insert" : ("br(freedomscientific):braillespacebar+dot8+dot2+dot4+dot7",),
+			"kb:delete" : ("br(freedomscientific):rightShiftKey", "br(freedomscientific):braillespacebar+dot1+dot5+dot4",),
+			"kb:shift+delete" : ("br(freedomscientific):braillespacebar+dot1+dot7+dot5+dot4",),
+			"kb:control+delete" : ("br(freedomscientific):dot1+dot7+dot5+dot4+braillespacebar+dot8",),
+			"kb:pause" : ("br(freedomscientific):dot3+dot2+dot1+leftshiftkey+dot4",),
+			"kb:shift+pause" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot4+leftshiftkey",),
+			"kb:control+break" : ("br(freedomscientific):dot3+dot2+dot1+dot7+dot4+dot8+leftshiftkey",),
+			"kb:alt+pause" : ("br(freedomscientific):dot3+dot2+dot1+dot4+dot8+leftshiftkey",),
+			"kb:applications" : ("br(freedomscientific):dot3+dot2+dot1+dot6+dot5+dot4+rightShiftKey", "br(freedomscientific):braillespacebar+dot3+dot2+dot1+dot4",),
 			"kb:shift+tab": ("br(freedomScientific):dot1+dot2+brailleSpaceBar",),
 			"kb:tab" : ("br(freedomScientific):dot4+dot5+brailleSpaceBar",),
 			"kb:upArrow" : ("br(freedomScientific):dot1+brailleSpaceBar",),
@@ -255,6 +427,16 @@ class BrailleDisplayDriver(braille.BrailleDisplayDriver,ScriptableObject):
 			"reportCurrentLine" : ("br(freedomScientific):dot1+dot4+brailleSpaceBar",),
 			"showGui" :("br(freedomScientific):dot1+dot3+dot4+dot5+brailleSpaceBar",),
 			"braille_toggleTether" : ("br(freedomScientific):leftGDFButton+rightGDFButton",),
+			"navigatorObject_toFocus" : ("br(freedomscientific):rightgdfbutton",),
+			"review_bottom" : ("br(freedomscientific):leftadvancebar+leftrockerbardown", "br(freedomscientific):rightadvancebar+rightrockerbardown", "br(freedomscientific):leftadvancebar+rightrockerbardown", "br(freedomscientific):rightadvancebar+leftrockerbardown",),
+			"review_top" : ("br(freedomscientific):leftrockerbarup+rightadvancebar", "br(freedomscientific):leftgdfbutton", "br(freedomscientific):leftrockerbarup+leftadvancebar", "br(freedomscientific):rightrockerbarup+rightadvancebar", "br(freedomscientific):rightrockerbarup+leftadvancebar",),
+			"review_endOfLine" : ("br(freedomscientific):rightadvancebar+rightgdfbutton", "br(freedomscientific):rightadvancebar+leftgdfbutton",),
+			"review_startOfLine" : ("br(freedomscientific):leftadvancebar+rightgdfbutton", "br(freedomscientific):leftadvancebar+leftgdfbutton",),
+			"reviewMode_previous" : ("br(freedomscientific):rightgdfbutton+leftrockerbardown", "br(freedomscientific):leftgdfbutton+rightrockerbardown",),
+			"reviewMode_next" : ("br(freedomscientific):rightrockerbarup+leftgdfbutton", "br(freedomscientific):leftrockerbarup+rightgdfbutton",),
+			"review_markStartForCopy" : ("br(freedomscientific):leftshiftkey+rightgdfbutton", "br(freedomscientific):leftshiftkey+leftgdfbutton",),
+			"review_copy" : ("br(freedomscientific):rightgdfbutton+rightShiftKey", "br(freedomscientific):leftgdfbutton+rightShiftKey",),
+			"toggleInputHelp" : ("br(freedomscientific):dot2+dot1+dot5+braillespacebar",),
 		}
 	})
 
